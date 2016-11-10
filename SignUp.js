13d5fc566d4797434ad29ac55e0fa1bd9ad76f9b
Luke Harris <lukeh13@hotmail.co.uk>
2016-11-10 17:06:52 +0000

    Implemented Firebase to store submitted emails in database

--- SignUp.js
+++ SignUp.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import {Form, FormGroup, FormControl, ControlLabel, Button, Alert} from "react-bootstrap";
+import * as firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyBKAraQB1qiQa5_QBWKb1ZyQaUeiEMf0fE",
+    authDomain: "technicaltest-66a28.firebaseapp.com",
+    databaseURL: "https://technicaltest-66a28.firebaseio.com",
+    storageBucket: "technicaltest-66a28.appspot.com",
+};
+firebase.initializeApp(config);
 
 export default class SignUp extends React.Component {
     constructor(props) {
@@ -12,7 +21,10 @@ export default class SignUp extends React.Component {
         this.setState({email: e.target.value});
     }
     signUpUser(event) {
-        this.setState({email: ""});
+        if (this.state.email != "" && this.state.email.indexOf("@") != -1) {
+            var storageRef = firebase.database().ref("SignedUpUsers").push(this.state.email);
+            this.setState({email: ""});
+        }
     }
     render() {
         return (
